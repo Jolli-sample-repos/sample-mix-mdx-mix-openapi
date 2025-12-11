14cdd5011811daca752e19679fc811b1160854bf
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 13:41:22 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -7,10 +7,10 @@ export default {
   'valid-configuration-guide': 'Valid - Configuration Guide',
   'valid-getting-started': 'Valid - Getting Started',
   'api-reference': {
-    title: 'API Reference',
+    title: 'LOOK AT THIS',
     type: 'menu',
     items: {
-      'valid-jolli-api-yaml': { title: 'Valid - Jolli API (YAML)', href: '/api-docs/valid-jolli-api-yaml' },
+      'valid-jolli-api-yaml': { title: 'ITS A CUSTOM TITLE!!!!', href: '/api-docs/valid-jolli-api-yaml' },
       'valid-pet-store-api-yaml': { title: 'Valid - Pet Store API (YAML)', href: '/api-docs/valid-pet-store-api-yaml' },
       'valid-jolli-api-json': { title: 'Valid - Jolli API (JSON)', href: '/api-docs/valid-jolli-api-json' },
       'valid-pet-store-api-json': { title: 'Valid - Pet Store API (JSON)', href: '/api-docs/valid-pet-store-api-json' }
