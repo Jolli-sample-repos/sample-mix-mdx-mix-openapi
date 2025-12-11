d2560145256bf9fb55b042678368a6d9a8c606a6
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 15:59:44 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -5,13 +5,5 @@ export default {
   'valid-advanced-features': 'Valid - Advanced Features',
   'valid-api-authentication': 'Valid - API Authentication',
   'valid-configuration-guide': 'Valid - Configuration Guide',
-  'valid-getting-started': 'Valid - Getting Started',
-  'api-reference': {
-    title: 'API Reference',
-    type: 'menu',
-    items: {
-      'valid-jolli-api-yaml': { title: 'Custom API Link In Menu', href: '/api-docs/valid-jolli-api-yaml' },
-      'valid-pet-store-api-yaml': { title: 'Valid - Pet Store API (YAML)', href: '/api-docs/valid-pet-store-api-yaml' }
-    }
-  }
+  'valid-getting-started': 'Valid - Getting Started'
 }
